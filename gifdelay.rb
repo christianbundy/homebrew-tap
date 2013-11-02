0f1c1bab7259d9a490c9271b5a747520995a5982
Christian Bundy <me@christianbundy.com>
2013-11-02 15:02:38 -0500

    Remove ffmpeg dependency from gifdelay

--- gifdelay.rb
+++ gifdelay.rb
@@ -2,7 +2,6 @@ require 'formula'
 
 class Gifdelay < Formula
   depends_on 'imagemagick'
-  depends_on 'ffmpeg'
   homepage 'https://github.com/ChristianBundy/gifdelay'
   url 'https://raw.github.com/ChristianBundy/gifdelay/master/bin/gifdelay'
   sha1 'd91cfd89b8f9523be6ba8878aa4d917ec60da647'
