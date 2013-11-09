6b71ca4970c3f41391ff7bf726b2451c849b3231
Christian Bundy <me@christianbundy.com>
2013-11-09 12:00:59 -0600

    Fix URL case for gifdelay

--- gifdelay.rb
+++ gifdelay.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Gifdelay < GithubGistFormula
   depends_on 'imagemagick'
-  homepage 'https://github.com/ChristianBundy/gifdelay'
-  url 'https://raw.github.com/ChristianBundy/gifdelay/master/bin/gifdelay'
+  homepage 'https://github.com/christianbundy/gifdelay'
+  url 'https://raw.github.com/christianbundy/gifdelay/master/bin/gifdelay'
   sha1 '5b31bff835c167b13e0f39a7fb6acd8430191a56'
 end
